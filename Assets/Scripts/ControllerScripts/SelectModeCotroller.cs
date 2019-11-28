6c185ed4925ef11f3109216c62d53f5a1e45387c
DangNgocDai <forever.one.love.myapple@gmail.com>
2019-11-28 23:16:41 +0700

    Tạo MainMenu, SelectMode

---
+++ Assets/Scripts/ControllerScripts/SelectModeCotroller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectModeCotroller : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Application.LoadLevel("SampleScene");
+    }
+    public void BackToMenu()
+    {
+        Application.LoadLevel("MainMenu");
+    }
+}
