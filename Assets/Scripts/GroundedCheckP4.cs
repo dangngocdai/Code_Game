32480cca1fc85c990e18a06f5f2594f80faa8522
dong <harrydong98vp@gmail.com>
2019-11-29 14:50:41 +0700

    tạo nv4+nv5

--- Assets/Scripts/GroundedCheckP4.cs
+++ Assets/Scripts/GroundedCheckP4.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class GroundedCheckP4 : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Player4 player;
     void Start()
     {
-        
+        player = gameObject.GetComponentInParent<Player4>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void OnTriggerEnter2D(Collider2D conlision)
     {
-        
+        player.grounded = true;
+    }
+
+    void OnTriggerStay2D(Collider2D conllision)
+    {
+        player.grounded = true;
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        player.grounded = false;
     }
 }
--- Assets/Scripts/GroundedCheckP5.cs
+++ Assets/Scripts/GroundedCheckP5.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class GroundedCheckP5 : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Player5 player;
     void Start()
     {
-        
+        player = gameObject.GetComponentInParent<Player5>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void OnTriggerEnter2D(Collider2D conlision)
     {
-        
+        player.grounded = true;
+    }
+
+    void OnTriggerStay2D(Collider2D conllision)
+    {
+        player.grounded = true;
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        player.grounded = false;
     }
 }
